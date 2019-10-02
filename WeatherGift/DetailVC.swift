c8970a5af53bc6e1bc65ede072815331d6bb186a
Anastasia Kyratzoglou <kyratzoa@bc.edu>
2019-10-02 15:17:40 -0400

    7.3 Completed - Introducing the UIPageViewController

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class DetailVC: UIViewController {
-
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+        locationLabel.text = locationsArray[currentPage]
+        }
     
 
 }
--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -10,21 +10,46 @@ import UIKit
 
 class PageVC: UIPageViewController {
 
+    var currentPage = 0
+    var locationsArray = ["Local City Weather", "Chestnuthill, MA", "Sydney, Australia", "Accra, Ghana", "Uglich, Russia"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        delegate = self
+        dataSource = self
+        
+        setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
+    }
 
-        // Do any additional setup after loading the view.
+    func createDetailVC(forPage page: Int)-> DetailVC{
+         currentPage = min(max(0, page), locationsArray.count - 1)
+        
+        let detailVC = storyboard!.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
+        
+        detailVC.currentPage = currentPage
+        detailVC.locationsArray = locationsArray
+      return detailVC
     }
     
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate{
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? DetailVC{
+            if currentViewController.currentPage < locationsArray.count - 1 {
+                return createDetailVC(forPage: currentViewController.currentPage + 1)
+            }
+        }
+        return nil
     }
-    */
-
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? DetailVC{
+            if currentViewController.currentPage > 0{
+                return createDetailVC(forPage: currentViewController.currentPage - 1)
+            }
+        }
+        return nil
+    }
 }
